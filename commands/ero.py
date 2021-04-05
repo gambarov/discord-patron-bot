113b4757b0a3aa59b7ef8a12c1ad1a896fef7d64
gambarov <gambarov.20015960@gmail.com>
2021-04-05 23:01:33 +0500

    ero spoiler photo

--- commands/ero.py
+++ commands/ero.py
@@ -31,7 +31,7 @@ class EroCommand(commands.Cog):
                         raise Exception('Could\'nt download file')
                     
                     data = io.BytesIO(await response.read())
-                    await ctx.send(file = discord.File(data, '{}.png'.format(tag if tag else 'ero'), spoiler = False))
+                    await ctx.send(file = discord.File(data, '{}.png'.format(tag if tag else 'ero'), spoiler = True))
 
     @execute.error
     async def on_error(self, ctx, error):
